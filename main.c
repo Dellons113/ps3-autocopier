ebab38a4091515864f746c006c58a7843d3ad6be
Dellons113 <109490368+Dellons113@users.noreply.github.com>
2026-06-09 08:10:41 +0700

    Refactor module to use sys calls instead of psl1ght

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
-#include <psl1ght/lv2.h>
-#include <psl1ght/lv2/modules.h>
-#include <psl1ght/lv2/fs.h>
-#include <psl1ght/lv2/thread.h>
+#include <sys/module.h>
+#include <sys/thread.h>
+#include <lv2/fs.h>
+#include <fcntl.h>
 #include <string.h>
 
-// Definisi Modul
-LV2_MODULE_INFO(restorer, 0, 1, 1);
-LV2_MODULE_START(restorer_start);
-LV2_MODULE_STOP(restorer_stop);
+// Definisi Modul Plugin
+SYS_MODULE_INFO(restorer, 0, 1, 1);
+SYS_MODULE_START(restorer_start);
+SYS_MODULE_STOP(restorer_stop);
 
-// Fungsi Salin (Menggunakan fungsi file system PSL1GHT)
+sys_thread_t thread_id;
+
+// Fungsi Salin (Menggunakan standar pembacaan PSL1GHT)
 void check_and_copy() {
     int fd;
-    // Cek file
-    if (lv2FsOpen("/dev_hdd0/tmp/wm_lang/LANG_ID.TXT", LV2_FS_O_RDONLY, &fd, NULL, 0) == 0) {
+    
+    // 1. Cek apakah file sudah ada di HDD
+    if (lv2FsOpen("/dev_hdd0/tmp/wm_lang/LANG_ID.TXT", O_RDONLY, &fd, 0, 0) == 0) {
         lv2FsClose(fd);
         return;
     }
 
+    // 2. Jika tidak ada, siapkan penyalinan dari dev_flash
     int fd_src, fd_dst;
     uint64_t read_bytes, write_bytes;
     char buffer[1024];
 
-    if (lv2FsOpen("/dev_flash/etc/brankas/LANG_ID.TXT", LV2_FS_O_RDONLY, &fd_src, NULL, 0) == 0) {
+    if (lv2FsOpen("/dev_flash/etc/brankas/LANG_ID.TXT", O_RDONLY, &fd_src, 0, 0) == 0) {
+        
         lv2FsMkdir("/dev_hdd0/tmp/wm_lang", 0777);
-        if (lv2FsOpen("/dev_hdd0/tmp/wm_lang/LANG_ID.TXT", LV2_FS_O_WRONLY | LV2_FS_O_CREAT | LV2_FS_O_TRUNC, &fd_dst, NULL, 0) == 0) {
+        
+        if (lv2FsOpen("/dev_hdd0/tmp/wm_lang/LANG_ID.TXT", O_WRONLY | O_CREAT | O_TRUNC, &fd_dst, 0, 0) == 0) {
             while (lv2FsRead(fd_src, buffer, sizeof(buffer), &read_bytes) == 0 && read_bytes > 0) {
                 lv2FsWrite(fd_dst, buffer, read_bytes, &write_bytes);
             }
@@ -34,15 +40,16 @@ void check_and_copy() {
     }
 }
 
-static void restorer_thread(uint64_t arg) {
+// Background Thread
+static void restorer_thread(void *arg) {
     check_and_copy();
-    lv2ThreadExit(0);
+    sysThreadExit(0);
 }
 
+// Eksekutor Saat Mesin Menyala
 int restorer_start(size_t args, void *argp) {
-    sys_ppu_thread_t thread_id;
-    lv2ThreadCreate(&thread_id, restorer_thread, 0, 1000, 0x1000, 0, "Restorer_Thread");
-    return 0; // LV2_MODULE_RESIDENT;
+    sysThreadCreate(&thread_id, restorer_thread, NULL, 1000, 0x1000, THREAD_JOINABLE, "Restorer_Thread");
+    return 0; 
 }
 
 int restorer_stop(void) {
